0c1434f2af36c83c386b3f70442c4e040acc0c20
samarthGit2023 <langaliasamarth@gmail.com>
2025-02-15 15:40:48 +0530

    Yeah boi, noice commit

--- src/java/com/idb/controller/AppController.java
+++ src/java/com/idb/controller/AppController.java
@@ -25,6 +25,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+// yeah baby, this for the commit bro.
+
 @Controller
 public class AppController {
 
